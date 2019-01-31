fcbe42a21af98ad9331e822bde34d729c56da1cd
qiye <web@tedu.cn>
2019-01-31 14:27:12 +0800

    globalSettings

--- src/views/GlobalSettings.vue
+++ src/views/GlobalSettings.vue
@@ -1,5 +1,80 @@
 <template>
-  <div class="">
-    <h1>GlobalSettings</h1>
+  <div class="settings">
+    <el-breadcrumb>
+      <el-breadcrumb-item to="/main">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>全局设置</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <br>
+    <el-card shadow="never">
+      <el-form label-width="110px" >
+      
+        <el-form-item label="应用名称：">
+          <el-input v-model="formData.appName"></el-input>
+        </el-form-item>
+
+        <el-form-item label="数据API地址：">
+          <el-input v-model="formData.apiUrl"></el-input>
+        </el-form-item>
+
+        <el-form-item label="管理后台地址：">
+          <el-input v-model="formData.adminUrl"></el-input>
+        </el-form-item>
+
+        <el-form-item label="客户App地址：">
+          <el-input v-model="formData.appUrl"></el-input>
+        </el-form-item>
+
+        <el-form-item label="ICP备案号：">
+          <el-input v-model="formData.icp"></el-input>
+        </el-form-item>
+
+        <el-form-item label="版权声明：">
+          <el-input v-model="formData.copyright"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="doSubmot">提交</el-button>
+          <el-button @click="doCancel">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      formData:{ }
+    }
+  },
+  mounted(){
+    //将$store存储中的全局配置数据赋值给中间变量-formData，方便表单元素执行双向数据绑定
+    // 引用复制,两个变量指向一个对象
+    // this.formData = this.$store.state.globalSettings;
+    // 对象复制,创建出两个一样的对象
+    this.formData={ };
+    this.formData.appName=this.$store.state.globalSettings.appName;
+    this.formData.appUrl=this.$store.state.globalSettings.appUrl;
+    this.formData.adminUrl=this.$store.state.globalSettings.adminUrl;
+    this.formData.apiUrl=this.$store.state.globalSettings.apiUrl;
+    this.formData.icp=this.$store.state.globalSettings.icp;
+    this.formData.copyright=this.$store.state.globalSettings.copyright;
+  },
+  methods:{
+    doSubmot(){
+
+    },
+    doCancel(){
+      this.formData={ };
+      this.formData.appName=this.$store.state.globalSettings.appName;
+      this.formData.appUrl=this.$store.state.globalSettings.appUrl;
+      this.formData.adminUrl=this.$store.state.globalSettings.adminUrl;
+      this.formData.apiUrl=this.$store.state.globalSettings.apiUrl;
+      this.formData.icp=this.$store.state.globalSettings.icp;
+      this.formData.copyright=this.$store.state.globalSettings.copyright;
+    }
+  }
+}
+</script>
